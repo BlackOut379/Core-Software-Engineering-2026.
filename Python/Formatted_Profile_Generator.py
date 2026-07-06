25101379b60c0a4a909229b8940a49977de76100
BlackOut379 <dhanrajkarkera16@gmail.com>
2026-07-06 20:58:46 +0530

    Create Formatted_Profile_Generator.py

---
+++ Python/Formatted_Profile_Generator.py
@@ -0,0 +1,10 @@
+def profile():
+    name = input("Enter First Name: ").strip().title()
+    surname = input("Enter Last Name: ").strip().title()
+    location = input("Enter Location: ").strip().title()
+    print(f"Name: {name} {surname} Location:  {location}")
+
+def main():
+    profile()
+    
+main()
