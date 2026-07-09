b7b01108dbb403b3583d4e6b5fc438112e4bb1ce
BlackOut379 <dhanrajkarkera16@gmail.com>
2026-07-09 20:41:33 +0530

    Create profile.py

---
+++ Python/Formatted_Profile/profile.py
@@ -0,0 +1,11 @@
+def profile():
+    name = input("Enter First Name: ").strip().title()
+    surname = input("Enter Last Name: ").strip().title()
+    location = input("Enter Location: ").strip().title()
+    print(f"Name: {name} {surname}")
+    print(f"Location: {location}")
+
+def main():
+    profile()
+
+main()
